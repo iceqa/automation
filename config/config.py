84a5650390d6bc1994fcc5d577e982b7abaf66ee
iceqa <88378782+iceqa@users.noreply.github.com>
2021-09-11 21:15:26 +0300

    remove JWT from project.

--- config/config.py
+++ config/config.py
@@ -5,20 +5,6 @@
 API_URL = "https://api.nexmo.com"
 
 
-# JSON Web Token to work with APIs.
-JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9" \
-      ".eyJpYXQiOjE2MzEzNjE5MjYsImV4cCI6MTY" \
-      "zMTg4MDMyNiwianRpIjoiNlRWcDRmdTRBamw" \
-      "yIiwiYXBwbGljYXRpb25faWQiOiJiYmVmMDN" \
-      "kNS0yYjdjLTQ4YzctYjhkZS1lOTkzZGVkMWZ" \
-      "jZGIifQ.gAEHf_frYR8FcyG5ptv8g1s7M15R" \
-      "308ljp6N5f74vWnJQxJ6nllVstEv0cfNrrWF" \
-      "GPyswwKSihTl5Kmcd0_Sa_vbDOTk55JMg8qm" \
-      "tMqpRdDLurB2q4FbyAFI0NtBlohgVfLHUNEG" \
-      "zVGPjuZ4cu44zhWztObLuHxqBM9E1WQGvFsE" \
-      "N2xOfkqilY8e_WLDIO-txxDp1Gqsg0DAUrcZ" \
-      "0mlFNJL2yktul9SJVQJUhBb48wa5gOGEhuuz" \
-      "ZqGgh7Fd6u2KzGLHes-t5UKIuHiyqs6oNlZo" \
-      "HRubIBMj1ocPS-AQr56NWDCTlrDrwPltm-zG" \
-      "I4GTVFiVwSLzxHLLbZR_c_gcig "
+# Add your JWT to this variable to work with APIs.
+JWT = ""
 
